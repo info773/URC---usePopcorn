524c3f31987833d91979a25d3f4f31f659795671
Berry <info@felix-behrens.de>
2026-01-01 11:57:23 +0100

    Split up in componentes and files each

---
+++ src/MovieList.js
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import { tempMovieData } from "./Main";
+
+export function MovieList() {
+  const [movies, setMovies] = useState(tempMovieData);
+
+  return (
+    <ul className="list">
+      {movies?.map((movie) => (
+        <Movie movie={movie} key={movie.imdbID} />
+      ))}
+    </ul>
+  );
+}
+function Movie({ movie }) {
+  return (
+    <li>
+      <img src={movie.Poster} alt={`${movie.Title} poster`} />
+      <h3>{movie.Title}</h3>
+      <div>
+        <p>
+          <span>🗓</span>
+          <span>{movie.Year}</span>
+        </p>
+      </div>
+    </li>
+  );
+}
